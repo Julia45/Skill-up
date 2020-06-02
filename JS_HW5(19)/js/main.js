83a4f0d0ab12721216996fa48da3f87c2a8e9c6a
Juliya Mankovskaya <juliyamankovskaya@gmail.com>
2020-06-02 16:56:19 +0300

    message

--- JS_HW5(19)/js/main.js
+++ JS_HW5(19)/js/main.js
@@ -2,107 +2,105 @@
 /* eslint-disable func-style */
 /* eslint-disable require-jsdoc */
 
-var mainContainer = null;
-var prevIndicator = null;
-var slidesCount = 5
+var prevIndicator;
+var slidesCount = 5;
 
 
-  function createMainContainer() {
-    var mainContainer = document.createElement('div');
+function createMainContainer() {
+  var mainContainer = document.createElement('div');
 
-    mainContainer.classList.add('carousel');
-    mainContainer.id = 'carousel';
-    document.body.append(mainContainer);
+  mainContainer.classList.add('carousel');
+  mainContainer.id = 'carousel';
+  document.body.append(mainContainer);
 
-  }
+}
 
-  function createSlides(slidesCount = 5) {
+function createSlides(slidesCount) {
 
-    var mainContainer = document.getElementById('carousel')
-    var listContainer = document.createElement('ul');
+  var mainContainer = document.getElementById('carousel')
+  var listContainer = document.createElement('ul');
 
-    listContainer.classList.add('slides')
+  listContainer.classList.add('slides')
 
-    for (i = 0; i < slidesCount; i++) {
-      var listElement = document.createElement('li');
-      var listElementLink = document.createElement('a');
+  for (var i = 0; i < slidesCount; i++) {
+    var listElement = document.createElement('li');
+    var listElementLink = document.createElement('a');
 
-      if (i === 0) {
-        listElement.setAttribute('class', 'slides__item active')
-      } else {
-        listElement.setAttribute('class', 'slides__item')
-      }
-
-      listElementLink.setAttribute('href', "#");
-      listElement.appendChild(listElementLink)
-      listContainer.appendChild(listElement);
+    if (i === 0) {
+      listElement.classList.add('slides__item', 'active')
+    } else {
+      listElement.classList.add('slides__item')
     }
 
-    mainContainer.appendChild(listContainer)
+    listElementLink.setAttribute('href', "#");
+    listElement.appendChild(listElementLink)
+    listContainer.appendChild(listElement);
   }
 
-
-  function createIndicators(slidesCount = 5) {
-
-
-    var mainContainer = document.getElementById('carousel')
-    var indicatorContainer = document.createElement('div');
-    indicatorContainer.classList.add('indicators');
-    for (i = 0; i < slidesCount; i++) {
-      var indicator = document.createElement('span');
-
-      if (i === 0) {
-        indicator.setAttribute('class', 'indicators__item active')
-      } else {
-        indicator.setAttribute('class', 'indicators__item')
-      }
+  mainContainer.appendChild(listContainer)
+}
 
 
-      indicator.setAttribute('data-slide-to', i);
-      indicatorContainer.appendChild(indicator);
+function createIndicators(slidesCount) {
+
+  var mainContainer = document.getElementById('carousel')
+  var indicatorContainer = document.createElement('div');
+  indicatorContainer.classList.add('indicators');
+  for (i = 0; i < slidesCount; i++) {
+    var indicator = document.createElement('span');
+
+    if (i === 0) {
+      indicator.classList.add('indicators__item', 'active')
+    } else {
+      indicator.classList.add('indicators__item')
     }
-    mainContainer.appendChild(indicatorContainer);
+
+    indicator.dataset.slideTo = i;
+    indicatorContainer.appendChild(indicator);
   }
+  mainContainer.appendChild(indicatorContainer);
+}
 
 
 
-  function createControls() {
-    var mainContainer = document.getElementById('carousel')
-    var controlsContainer = document.createElement('div');
-    controlsContainer.classList.add('controls');
-    for (i = 0; i < 3; i++) {
-      var controlbutton = document.createElement('div');
-      var controlIcon = document.createElement('i');
+function createControls() {
+  var mainContainer = document.getElementById('carousel')
+  var controlsContainer = document.createElement('div');
+  controlsContainer.classList.add('controls');
 
-      switch (i) {
-        case 0:
-          controlbutton.classList.add('controls__item', 'controls__prev');
-          controlbutton.innerHTML = 'Prev'
-          controlIcon.classList.add('fas', 'fa-chevron-left');
-          break;
-        case 1:
-          controlbutton.classList.add('controls__item', 'controls__next');
-          controlIcon.classList.add('fas', 'fa-chevron-right');
-          controlbutton.innerHTML = 'Next'
-          break;
-        case 2:
-          controlbutton.classList.add('controls__item', 'controls__pause');
-          controlIcon.classList.add('fas', 'fa-play');
-          controlbutton.innerHTML = 'Pause'
-          break;
-      }
-      controlbutton.appendChild(controlIcon);
-      controlsContainer.appendChild(controlbutton);
+  for (i = 0; i < 3; i++) {
+    var controlbutton = document.createElement('div');
+    var controlIcon = document.createElement('i');
+
+    switch (i) {
+      case 0:
+        controlbutton.classList.add('controls__item', 'controls__prev');
+        controlbutton.innerHTML = 'Prev'
+        controlIcon.classList.add('fas', 'fa-chevron-left');
+        break;
+      case 1:
+        controlbutton.classList.add('controls__item', 'controls__next');
+        controlIcon.classList.add('fas', 'fa-chevron-right');
+        controlbutton.innerHTML = 'Next'
+        break;
+      case 2:
+        controlbutton.classList.add('controls__item', 'controls__pause');
+        controlIcon.classList.add('fas', 'fa-play');
+        controlbutton.innerHTML = 'Pause'
+        break;
     }
-    mainContainer.appendChild(controlsContainer);
+    controlbutton.appendChild(controlIcon);
+    controlsContainer.appendChild(controlbutton);
   }
+  mainContainer.appendChild(controlsContainer);
+}
 
 
 
-  function createStyle() {
-    var mainContainer = document.getElementById('carousel')
-    styleContainer = document.createElement('style');
-    var style = `
+function createStyle() {
+  var mainContainer = document.getElementById('carousel')
+  var styleContainer = document.createElement('style');
+  styleContainer.innerHTML =  `
     .controls,
     .slides {
       position: relative;
@@ -119,31 +117,42 @@ var slidesCount = 5
       border-radius: 4px;
     }`;
 
-    styleContainer.innerHTML = style;
-    mainContainer.appendChild(styleContainer);
-  }
+  mainContainer.appendChild(styleContainer);
+}
 
-  function clickHandler(e) {
-    var target = e.target;
 
-    if (target.classList.contains('indicators__item')) {
-      target.style.backgroundColor = 'red';
+function clickHandler(e) {
+  var target = e.target;
 
-      if (prevIndicator !== null) prevIndicator.removeAttribute('style');
-      
 
-      prevIndicator = target;
+  if (target.classList.contains('indicators__item')) {
+    // target.style.backgroundColor = 'red';
+
+    if (target.style.backgroundColor !== 'red') {
+      target.style.backgroundColor = 'red'
+    } else {
+      target.removeAttribute('style')
     }
+
+    if (prevIndicator && prevIndicator !== target) {
+      prevIndicator.removeAttribute('style');
+    }
+
+    
+
+    prevIndicator = target;
   }
+}
 
-  function eventListenerFunc() {
-    var indicatorContainer = document.querySelector('div.indicators');
+function eventListenerFunc() {
+  var indicatorContainer = document.querySelector('.indicators');
 
-    indicatorContainer.addEventListener('click', clickHandler);
+  indicatorContainer.addEventListener('click', clickHandler);
 
-  }
 
-  function createCarousel(slidesCount) {
+}
+
+function createCarousel(slidesCount) {
   createMainContainer()
   createSlides(slidesCount);
   createIndicators(slidesCount)
@@ -152,5 +161,5 @@ var slidesCount = 5
   eventListenerFunc()
 }
 
-createCarousel();
+createCarousel(slidesCount);
 
